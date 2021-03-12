76df194c6b1898f22a1e9e2d8e9c9adca394bf74
Ananev Andrei <ananevandrei11@gmail.com>
2021-03-12 21:40:43 +0400

    #2 12/03/21 _ 20:30 MSK

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 let phoneCall = document.getElementById('phoneCall');
 let phoneService = document.getElementById('phoneService');
+let phoneLetter = document.getElementById('phoneLetter');
 let maskOptionsPhone = {
   mask: '+{7}(000)000-00-00'
 };
 let mask1 = IMask(phoneService, maskOptionsPhone);
 let mask2 = IMask(phoneCall, maskOptionsPhone);
+let mask3 = IMask(phoneLetter, maskOptionsPhone);
